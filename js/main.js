8318869ed9dad2b14dbdb837463824c364cf378a
Nic Nilov <nic@nicnilov.com>
2013-06-09 22:27:21 +0400

    page title update implemented

--- js/main.js
+++ js/main.js
@@ -6,30 +6,37 @@
         switch (window.location.hash) {
             case "#news": {
                 $('#sct-news').show();
+                window.document.title = "Nic Nilov | News";
                 break;
             }
             case "#blog": {
                 $('#sct-blog').show();
+                window.document.title = "Nic Nilov | Blog";
                 break;
             }
             case "#projects": {
                 $('#sct-projects').show();
+                window.document.title = "Nic Nilov | Projects";
                 break;
             }
             case "#about": {
                 $('#sct-about').show();
+                window.document.title = "Nic Nilov | About me";
                 break;
             }
             case "#contact": {
                 $('#sct-contact').show();
+                window.document.title = "Nic Nilov | Contact";
                 break;
             }
             case "#resume": {
                 $('#sct-resume').show();
+                window.document.title = "Nic Nilov | Resume";
                 break;
             }
             default: {
                 $('#sct-landing').show();
+                window.document.title = "Nic Nilov | Personal website";
                 break;
             }
         }
